711fc199dedcfe5bb0956905a067796210bda59d
Nitin <Nitin>
2022-02-12 22:10:21 +0530

    first initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Nitin
+         Nitin's Hello
         </p>
         <a
           className="App-link"
