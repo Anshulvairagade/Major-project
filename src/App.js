6093fc60d87a40aa146a9b54f0ee2b87a06ad901
Himanshu956 <kamanehimanshu76@gmail.com>
2022-02-12 22:05:00 +0530

    heading change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Nitin's Hello
+         Nitin's namaste
         </p>
         <a
           className="App-link"
