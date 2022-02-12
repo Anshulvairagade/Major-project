068f775ba3ba302b857ab10452a115129f3dae47
Nitin <Nitin>
2022-02-12 22:17:01 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          First initial commit
+          small change
         </a>
       </header>
     </div>
