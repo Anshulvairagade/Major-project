bfdccb52bfae0b351b932ce6487a4701fbfbfb7f
Himanshu956 <kamanehimanshu76@gmail.com>
2022-02-13 13:39:42 +0530

    Lama video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Nitin's namaste
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          small change
-        </a>
-      </header>
-    </div>
-  );
+  return <Home/>
+    
+
 }
 
 export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,21 @@
+import "./topbar.css"
+import { } from "@mui/icons-material"
+
+export default function Topbar() {
+  return (
+    <div classname="topbarContainer">
+
+      <div className="topbarLeft">
+        <span className="log">Lamasocial</span>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchbar"></div>
+      </div>
+
+      <div className="topbarRight"></div>
+
+    </div>
+  )
+}
+
